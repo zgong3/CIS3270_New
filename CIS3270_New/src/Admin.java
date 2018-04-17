b9d8a9a249ce885ff83372b65a80ce22f6df8a4c
haley <haley@10.250.91.153>
2018-04-17 15:29:12 -0400

    Update

---
+++ CIS3270_New/src/Admin.java
@@ -0,0 +1,14 @@
+
+public class Admin extends Customer {
+
+	public static void main (String[] args){
+		
+		Customer a = new Customer();
+		a.setFirstName("Haley");
+		a.getFirstName();
+		System.out.println(a.firstName);
+		
+	}
+	
+
+}
---
+++ CIS3270_New/src/Customer.java
@@ -0,0 +1,110 @@
+
+
+public class Customer {
+
+	 String firstName;
+	private String lastName;
+	private String address;
+	private int zip;
+	private String state;
+	private String username;
+	private String password;
+	private String email;
+	private String SSN;
+	private String securityQuestion;
+	private String securityAnswer;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSSN() {
+		return SSN;
+	}
+
+	public void setSSN(String sSN) {
+		SSN = sSN;
+	}
+
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
+	
+	
+	
+	
+}
+
+
